c4f9d01a93c5c658e4f9d47c89e031f7e1b8b62f
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-12-21 09:40:47 +0000

    ui設定

--- app/app/EditMenu.tsx
+++ app/app/EditMenu.tsx
@@ -13,14 +13,17 @@ export default function EditMenu({
   setFileContent: (fileContent: fileType) => void;
 }) {
   return (
-    <div className="flex flex-col h-full">
+    <div className="flex flex-col h-full p-4 gap-4">
       <div className="flex-none">
         <input
           type="text"
           defaultValue={title}
+          className="w-full p-4 rounded bg-gray-100 border border-gray-800"
+          placeholder="ファイル名を入力してください"
           onChange={(e) => setTitle(e.target.value)}
         />
       </div>
+      <div className="flex-1 overflow-x-scroll flex flex-col gap-4 bg-gray-100 rounded"></div>
     </div>
   );
 }
