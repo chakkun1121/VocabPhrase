5c6985ad0670d0a1349ede66d73b692e70cf3524
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-12-21 08:40:57 +0000

    ui変更

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,15 @@
 import HeaderAppTitle from "./headerAppTitle";
-import HeaderAppLink from "./HeaderAppLink";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="flex p-4 bg-primary-200">
       <HeaderAppTitle />
-      <HeaderAppLink />
+      <div className="flex-none items-center flex">
+        <Link href="/app" className="">
+          単語帳を使う
+        </Link>
+      </div>
     </header>
   );
 }
--- app/app/_components/header.tsx
+++ app/app/_components/header.tsx
@@ -1,10 +1,11 @@
 import HeaderAppTitle from "@/app/_components/headerAppTitle";
+import HeaderUserMenu from "@/app/_components/headerUserMenu";
 
 export default function Header() {
   return (
     <header className="flex flex-none p-4 bg-primary-200">
       <HeaderAppTitle />
-      <div className="flex-none"></div>
+      <HeaderUserMenu />
     </header>
   );
 }
