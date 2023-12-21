41814e68cbc24ac4050d0c12e98ed3a09ab04799
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-12-21 05:51:44 +0000

    タイトルのリンクを縮小
    誤タップ減少

--- app/_components/headerAppTitle.tsx
+++ app/_components/headerAppTitle.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function HeaderAppTitle() {
   return (
-    <div className="flex-1">
+    <div className="flex-1 flex">
       <Link
         href="/"
         aria-label="ホームへ"
