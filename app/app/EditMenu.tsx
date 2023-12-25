91b4791c5b744309bc82905c363c901eb0640116
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-12-25 23:26:29 +0000

    ライブラリを入れてみた

--- app/app/EditMenu.tsx
+++ app/app/EditMenu.tsx
@@ -3,13 +3,32 @@ import { fileType } from "@/@types/fileType";
 import { uuidv7 as createUUID } from "uuidv7";
 import { MdDeleteOutline } from "react-icons/md";
 import { FaPlus } from "react-icons/fa";
-import {
-  ButtonHTMLAttributes,
-  DetailedHTMLProps,
-  useEffect,
-  useState,
-} from "react";
+import { LuGripVertical } from "react-icons/lu";
+import { ButtonHTMLAttributes, DetailedHTMLProps, useState } from "react";
+import { useSortable } from "@dnd-kit/sortable";
+
 import ImportForm from "./ImportForm";
+import {
+  DndContext,
+  DragOverlay,
+  closestCorners,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  UniqueIdentifier,
+  DragStartEvent,
+  DragOverEvent,
+  DragEndEvent,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+  rectSortingStrategy,
+  SortableContext,
+} from "@dnd-kit/sortable";
 
 export default function EditMenu({
   title,
@@ -23,6 +42,24 @@ export default function EditMenu({
   setFileContent: (fileContent: fileType) => void;
 }) {
   const [isShowImportBox, setIsShowImportBox] = useState(false);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+    if (!over) return;
+    if (active.id !== over.id) {
+      const oldIndex = fileContent.content.findIndex((v) => v.id === active.id);
+      const newIndex = fileContent.content.findIndex((v) => v.id === over.id);
+      setFileContent({
+        content: arrayMove(fileContent.content, oldIndex, newIndex),
+      });
+    }
+  };
   return (
     <div className="flex flex-col h-full p-4 gap-4">
       <div className="flex-none">
@@ -35,58 +72,23 @@ export default function EditMenu({
         />
       </div>
       <div className="flex-1 overflow-x-scroll flex flex-col gap-4 bg-gray-100 rounded p-2">
-        {fileContent.content.map((field, index) => (
-          <div
-            key={field.id}
-            className="flex gap-4 p-4 bg-gray-200 rounded items-center"
-          >
-            <div className="flex-none flex items-center">
-              <p>{index + 1}</p>
-            </div>
-            <div className="flex flex-col gap-4 flex-1">
-              <input
-                className="rounded p-2"
-                placeholder="英文を入力"
-                defaultValue={field.en}
-                onChange={(e) =>
-                  setFileContent({
-                    ...fileContent,
-                    content: fileContent.content.map((field, i) =>
-                      i === index ? { ...field, en: e.target.value } : field
-                    ),
-                  })
-                }
+        <DndContext
+          sensors={sensors}
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+        >
+          <SortableContext items={fileContent.content}>
+            {fileContent.content.map((field, index) => (
+              <Content
+                key={field.id}
+                field={field}
+                index={index}
+                fileContent={fileContent}
+                setFileContent={setFileContent}
               />
-              <input
-                className="rounded p-2"
-                placeholder="日本語訳を入力"
-                defaultValue={field.ja}
-                onChange={(e) =>
-                  setFileContent({
-                    content: fileContent.content.map((field, i) =>
-                      i === index ? { ...field, ja: e.target.value } : field
-                    ),
-                  })
-                }
-              />
-            </div>
-            <div className="flex-none flex flex-col gap-4">
-              <Button
-                onClick={() =>
-                  setFileContent({
-                    content: fileContent.content.filter((_, i) => i !== index),
-                  })
-                }
-                title="削除"
-              >
-                <MdDeleteOutline />
-              </Button>
-              {/* <Button>
-                <LuGripVertical />
-              </Button> */}
-            </div>
-          </div>
-        ))}
+            ))}
+          </SortableContext>
+        </DndContext>
         {fileContent.content.length === 0 && isShowImportBox && (
           <ImportForm
             close={() => setIsShowImportBox(false)}
@@ -119,6 +121,74 @@ export default function EditMenu({
             </button>
           </div>
         )}
+      </div>
+    </div>
+  );
+}
+function Content({
+  field,
+  index,
+  fileContent,
+  setFileContent,
+}: {
+  field: { id: UniqueIdentifier; en: string; ja: string };
+  index: number;
+  fileContent: fileType;
+  setFileContent: (fileContent: fileType) => void;
+}) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: field.id });
+  return (
+    <div
+      className="flex gap-4 p-4 bg-gray-200 rounded items-center"
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+    >
+      <div className="flex-none flex items-center">
+        <p>{index + 1}</p>
+      </div>
+      <div className="flex flex-col gap-4 flex-1">
+        <input
+          className="rounded p-2"
+          placeholder="英文を入力"
+          defaultValue={field.en}
+          onChange={(e) =>
+            setFileContent({
+              ...fileContent,
+              content: fileContent.content.map((field, i) =>
+                i === index ? { ...field, en: e.target.value } : field
+              ),
+            })
+          }
+        />
+        <input
+          className="rounded p-2"
+          placeholder="日本語訳を入力"
+          defaultValue={field.ja}
+          onChange={(e) =>
+            setFileContent({
+              content: fileContent.content.map((field, i) =>
+                i === index ? { ...field, ja: e.target.value } : field
+              ),
+            })
+          }
+        />
+      </div>
+      <div className="flex-none flex flex-col gap-4">
+        <Button
+          onClick={() =>
+            setFileContent({
+              content: fileContent.content.filter((_, i) => i !== index),
+            })
+          }
+          title="削除"
+        >
+          <MdDeleteOutline />
+        </Button>
+        <Button>
+          <LuGripVertical />
+        </Button>
       </div>
     </div>
   );
