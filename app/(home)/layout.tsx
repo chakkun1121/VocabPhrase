ec06b38b74f82631e0bd7622d9c0a3d0f852ccac
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-12-25 02:00:57 +0000

    トップページ作成

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,11 +1,35 @@
 import Header from "@/app/_components/header";
+import Link from "next/link";
 import { ReactNode } from "react";
 
 export default function HomeLayout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <div className="flex flex-col gap-4 h-screen">
       <Header />
-      <main>{children}</main>
-    </>
+      <main className="flex-1">{children}</main>
+      <footer className="flex-none text-center p-4 bg-BahamaBlue-700 text-white justify-center">
+        <p>© 2023 chakkun1121</p>
+        <div className="flex justify-center gap-4">
+          <Link
+            className="text-white hover:text-white visited:text-white"
+            href=""
+          >
+            利用規約
+          </Link>
+          <Link
+            className="text-white hover:text-white visited:text-white"
+            href=""
+          >
+            プライバシーポリシー
+          </Link>
+          <Link
+            className="text-white hover:text-white visited:text-white"
+            href=""
+          >
+            お問い合わせ
+          </Link>
+        </div>
+      </footer>
+    </div>
   );
 }
