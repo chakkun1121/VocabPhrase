cdce22444009a04ca793a4698ab9d7548bb5562c
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-01-25 04:15:53 +0000

    ページの追加
    まだ書きかけ

--- app/_components/helpKeyShortCut.tsx
+++ app/_components/helpKeyShortCut.tsx
@@ -3,9 +3,19 @@ import { useHotkeys } from "react-hotkeys-hook";
 
 export default function HelpKeyShortCut() {
   useHotkeys(
-    "ctrl+/,f1,?,shift+/",
+    "f1,?,shift+/",
     () => {
       window.open("/help", "_blank");
+    },
+    {
+      preventDefault: true,
+      enableOnFormTags: ["INPUT", "TEXTAREA", "SELECT"],
+    }
+  );
+  useHotkeys(
+    "ctrl+/",
+    () => {
+      window.open("/help/keyboard", "_blank");
     },
     {
       preventDefault: true,
