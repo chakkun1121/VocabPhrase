e37a5052ae2563cc5fdff2a59f0efd2e1a317355
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-01-25 03:54:52 +0000

    エラー解消

--- app/(home)/help/page.tsx
+++ app/(home)/help/page.tsx
@@ -8,29 +8,37 @@ export default async function HelpTop() {
       path.join(process.cwd(), "app", "(home)", "(mdx_documents)", "help")
     )
   ).filter((file) => !file.includes("."));
+  files.sort(); //文字列のみなので比較関数は不要
+  const metadatas: {
+    title: string;
+    description: string;
+  }[] = await Promise.all(
+    files.map(async (file) => {
+      const { metadata } = await import(
+        `@/app/(home)/(mdx_documents)/help/${file}/page.mdx`
+      );
+      return metadata;
+    })
+  );
   return (
     <section>
       <h2> ヘルプ記事一覧 </h2>
       <ul>
-        {files.map(async (file) => {
-          const { metadata } = await import(
-            `@/app/(home)/(mdx_documents)/help/${file}/page.mdx`
-          );
-          return (
-            <li key={file}>
-              <Link href={`/help/${file.replace(/\.mdx$/, "")}`}>
-                {metadata?.title || ""}
-              </Link>
-            </li>
-          );
-        })}
+        {files.map((file: string, i: number) => (
+          <li key={file}>
+            <Link href={`/help/${file.replace(/\.mdx$/, "")}`}>
+              {metadatas[i]?.title || ""}
+            </Link>
+          </li>
+        ))}
       </ul>
     </section>
   );
 }
 export const metadata: Metadata = {
   title: "ヘルプページトップ",
-  description: "英文、英単語専用の単語帳アプリVocabPhraseのヘルプページトップです。",
+  description:
+    "英文、英単語専用の単語帳アプリVocabPhraseのヘルプページトップです。",
   alternates: {
     canonical: "/help",
   },
