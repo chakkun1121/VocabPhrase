f374aa72fb238afd8979bac734272a6dda1c91c4
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-01-29 18:47:24 +0900

    文字選択中のメニューを作成

--- app/(app)/_components/rightClick/main.tsx
+++ app/(app)/_components/rightClick/main.tsx
@@ -1,29 +1,85 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-
+type menuInfo = {
+  [key: string]: menuInfoOptions[];
+};
+type menuInfoOptions = {
+  name: {
+    ja: string;
+    en: string;
+  };
+  onclick?: (text: string) => void;
+  options?: menuInfoOptions[];
+  when?: "onSelected";
+};
 export default function RightClick() {
   const [isShow, setIsShow] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const menu = [
-    {
-      name: {
-        ja: "印刷",
-        en: "print",
+  const pathName = usePathname();
+  const menu: menuInfo = {
+    default: [
+      {
+        name: {
+          ja: "コピー",
+          en: "Copy",
+        },
+        onclick: (text: string) => {
+          navigator.clipboard.writeText(text);
+        },
+        when: "onSelected",
       },
-      onclick: () => {},
-    },
-    {
-      name: {
-        ja: "ヘルプ",
-        en: "Help",
+      {
+        name: {
+          ja: "読み上げ",
+          en: "speech",
+        },
+        onclick: (text: string) => {
+          const utterance = new SpeechSynthesisUtterance(text);
+          utterance.lang = "en-US";
+          speechSynthesis.speak(utterance);
+        },
+        when: "onSelected",
       },
-      onclick: () => {
-        window.open("/help", "_blank");
+      {
+        name: {
+          ja: "で検索",
+          en: "Search with",
+        },
+        options: [
+          {
+            name: {
+              ja: "Google",
+              en: "Google",
+            },
+            onclick: (text: string) => {
+              window.open(`https://www.google.com/search?q=${text}`, "_blank");
+            },
+          },
+        ],
+        when: "onSelected",
       },
-    },
-  ];
+      {
+        name: {
+          ja: "印刷",
+          en: "print",
+        },
+        onclick: () => {},
+      },
+      {
+        name: {
+          ja: "ヘルプ",
+          en: "Help",
+        },
+        onclick: () => {
+          window.open("/help", "_blank");
+        },
+      },
+    ],
+  };
+
   useEffect(() => {
     window.addEventListener("contextmenu", (e) => {
       e.preventDefault();
@@ -32,6 +88,13 @@ export default function RightClick() {
       setIsShow(true);
     });
     window.addEventListener("click", () => setIsShow(false));
+  });
+  const onSelected = !!window.getSelection()?.toString();
+  const currentMenu = (menu[pathName] || menu.default).filter((e) => {
+    if (e.when === "onSelected") {
+      return onSelected;
+    }
+    return true;
   });
   return (
     <>
@@ -48,10 +111,13 @@ export default function RightClick() {
               left: x,
             }}
           >
-            {menu.map((e) => (
+            {currentMenu.map((e) => (
               <button
                 className="w-full h-12 px-4 text-left hover:bg-gray-200"
-                onClick={e.onclick}
+                onClick={() => {
+                  e.onclick?.(window.getSelection()?.toString() || "");
+                  setIsShow(false);
+                }}
                 key={e.name["en"]}
               >
                 {e.name["ja"]}
