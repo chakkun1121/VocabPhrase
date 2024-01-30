d0a0bb8c7e3387e48c379594f89c2f7fa834958c
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-01-30 04:03:38 +0000

    設定ページを準備

--- @types/settings.ts
+++ @types/settings.ts
@@ -1,2 +1,7 @@
-export type serverSettingsType = {};
+export type serverSettingsType = {
+  customSearch: {
+    name: string;
+    url: string; //検索キーワードが%sに入る
+  }[];
+};
 export type localSettings = {};
