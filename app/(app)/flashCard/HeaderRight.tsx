2664a97ddb12bbddd598052b4c3f72af9ae26ed7
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-01-31 23:38:02 +0000

    make exit button

--- app/(app)/flashCard/HeaderRight.tsx
+++ app/(app)/flashCard/HeaderRight.tsx
@@ -1,43 +1,43 @@
 "use client";
-import { ReactNode, useState } from "react";
-import { CiSettings } from "react-icons/ci";
+import { useState } from "react";
 import { CgMaximizeAlt } from "react-icons/cg";
-import { MdOutlineZoomInMap } from "react-icons/md";
+import { MdLogout, MdOutlineZoomInMap } from "react-icons/md";
 
-export default function HeaderRight() {
+export default function HeaderRight({
+  mode,
+  setMode,
+}: {
+  mode: "home" | "cards" | "result";
+  setMode: (mode: "home" | "cards" | "result") => void;
+}) {
   const [isMaximized, setIsMaximized] = useState(false);
   document.addEventListener("fullscreenchange", () => {
     setIsMaximized((prev) => !prev);
   });
+
   return (
     <nav className="gap-4 flex">
-      {(
-        [
-          {
-            title: isMaximized ? "戻す" : "最大化",
-            icon: isMaximized ? <MdOutlineZoomInMap /> : <CgMaximizeAlt />,
-            onClick: () => {
-              isMaximized
-                ? document.exitFullscreen()
-                : document.documentElement.requestFullscreen();
-              setIsMaximized((prev) => !prev);
-            },
-          },
-        ] as {
-          title: string;
-          icon: ReactNode;
-          onClick?: (e: any) => void;
-        }[]
-      ).map(({ title, icon, onClick }, i) => (
+      {mode == "cards" && (
         <button
-          key={i}
-          onClick={onClick}
-          className="gap-2 p-2  bg-primary-300 hover:bg-primary-400 rounded-full w-12 h-12 grid place-items-center"
-          title={title}
+          className="gap-2 p-2 bg-primary-300 hover:bg-primary-400 rounded-full w-12 h-12 grid place-items-center"
+          onClick={() => setMode("result")}
+          title="終了"
         >
-          {icon}
+          <MdLogout />
         </button>
-      ))}
+      )}
+      <button
+        className="gap-2 p-2 bg-primary-300 hover:bg-primary-400 rounded-full w-12 h-12 grid place-items-center"
+        onClick={() => {
+          isMaximized
+            ? document.exitFullscreen()
+            : document.documentElement.requestFullscreen();
+          setIsMaximized((prev) => !prev);
+        }}
+        title={isMaximized ? "戻す" : "最大化"}
+      >
+        {isMaximized ? <MdOutlineZoomInMap /> : <CgMaximizeAlt />}
+      </button>
     </nav>
   );
 }
