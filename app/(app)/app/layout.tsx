584810e5f35215d1ad9b1b842c44190f88f11d41
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-02-01 00:34:22 +0000

    fix style

--- app/(app)/app/layout.tsx
+++ app/(app)/app/layout.tsx
@@ -5,13 +5,13 @@ import { Metadata } from "next";
 
 export default function AppLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="h-screen">
+    <>
       <Header />
       <div className="flex mt-20">
         <LeftBar />
         <div className="flex-1 md:w-auto">{children}</div>
       </div>
-    </div>
+    </>
   );
 }
 export const metadata: Metadata = {
