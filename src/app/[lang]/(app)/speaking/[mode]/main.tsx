1493d0d5dac875c7440e44508552174dc73c3b81
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-02-16 07:50:57 +0000

    change style

--- src/app/[lang]/(app)/speaking/[mode]/main.tsx
+++ src/app/[lang]/(app)/speaking/[mode]/main.tsx
@@ -3,7 +3,8 @@
 import { useFile } from "@/googledrive/useFile";
 import { customSession } from "@/types/customSession";
 import { useSession } from "next-auth/react";
-import { SpeakingMode } from "../menu";
+import { SpeakingMode, speakingMode } from "../menu";
+import { useState } from "react";
 
 export default function Speaking({
   fileId,
@@ -18,9 +19,58 @@ export default function Speaking({
     useSession() as unknown as { data: customSession };
   const token = session?.accessToken;
   const { title, fileContent, loading } = useFile(token, fileId);
+  const [isShowEn, setIsShowEn] = useState(
+    speakingMode.find((m) => m.id === mode)?.defaultShow[lang] ?? true
+  );
+  const [isShowJa, setIsShowJa] = useState(
+    speakingMode.find((m) => m.id === mode)?.defaultShow[lang] ?? true
+  );
+  if (loading) return <p className="text-center">loading...</p>;
   return (
     <>
-      <p>Speaking</p>
+      <nav className="bg-gray-200 p-2 rounded [&>label]:block grid gap-2">
+        <label>
+          <input
+            type="checkbox"
+            checked={isShowEn}
+            onChange={(e) => setIsShowEn(e.target.checked)}
+          />
+          英文を表示する
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={isShowJa}
+            onChange={(e) => setIsShowJa(e.target.checked)}
+          />
+          日本語訳を表示する
+        </label>
+      </nav>
+      <div className="bg-primary-100 p-2 rounded">
+        <div>
+          <h2>{title.split(".").slice(0, -1).join(".")}</h2>
+        </div>
+        <div className="grid gap-4">
+          {fileContent?.content.map((content, index) => (
+            <div key={index} className="bg-primary-100 p-2 rounded">
+              <div>
+                {isShowEn ? (
+                  <p className="select-text">{content.en}</p>
+                ) : (
+                  <p className="text-gray-600">英文非表示</p>
+                )}
+              </div>
+              <div>
+                {isShowJa ? (
+                  <p className="select-text">{content.ja}</p>
+                ) : (
+                  <p className="text-gray-600">日本語訳非表示</p>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
--- src/app/[lang]/(app)/speaking/menu.ts
+++ src/app/[lang]/(app)/speaking/menu.ts
@@ -2,6 +2,7 @@ export const speakingMode: {
   id: string;
   name: { ja: string; en: string };
   description: { ja: string; en: string };
+  defaultShow: { ja: boolean; en: boolean };
 }[] = [
   {
     id: "repeat",
@@ -9,6 +10,10 @@ export const speakingMode: {
     description: {
       ja: "流れてきた英文に続いて読む練習です。",
       en: "Repeat the English sentence that comes out.",
+    },
+    defaultShow: {
+      ja: true,
+      en: true,
     },
   },
   {
@@ -18,6 +23,10 @@ export const speakingMode: {
       ja: "流れてきた英文と同時に読む練習です。",
       en: "Practice reading at the same time as the English text that comes to you.",
     },
+    defaultShow: {
+      ja: true,
+      en: true,
+    },
   },
   {
     id: "ja2en",
@@ -25,6 +34,10 @@ export const speakingMode: {
     description: {
       ja: "流れてきた日本語を英語に翻訳して読む練習です。",
       en: "Practice translating Japanese into English and reading it.",
+    },
+    defaultShow: {
+      ja: false,
+      en: false,
     },
   },
 ];
