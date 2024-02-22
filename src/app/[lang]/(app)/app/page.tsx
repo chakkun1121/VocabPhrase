9020a64882cfa8d9a63f1d8170c415d9c2ec3551
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-02-22 07:33:03 +0000

    google driveから開く機能を作成

--- src/app/[lang]/(app)/app/page.tsx
+++ src/app/[lang]/(app)/app/page.tsx
@@ -1,12 +1,15 @@
 import Print from "@/components/functional/print";
 import FileMenu from "./_components/edit/main";
 import HowToUse from "./howToUse";
+import { redirect } from "next/navigation";
 
 export default function App({
-  searchParams: { fileId },
+  searchParams: { fileId, state },
 }: {
-  searchParams: { fileId?: string };
+  searchParams: { fileId?: string; state?: string };
 }) {
+  const stateFileId = JSON.parse(state || "{}")?.ids?.[0];
+  if (stateFileId) redirect(`./app?fileId=${stateFileId}`);
   return fileId ? (
     <>
       <FileMenu fileId={fileId} key={fileId} />
