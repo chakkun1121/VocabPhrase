738bb812eccbf7462a6f36386f58a4694594c271
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-03-01 04:01:17 +0000

    set login page
    but it isn't move on github-codespaces

--- src/app/[lang]/login/LoginPage.tsx
+++ src/app/[lang]/login/LoginPage.tsx
@@ -1,31 +1,37 @@
-"use client";
-import { signIn, useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function LoginPage({ redirectTo }: { redirectTo: string }) {
-  const { status } = useSession();
   return (
     <>
-      <div className="flex flex-col items-center">
+      <form
+        className="flex flex-col items-center"
+        action={async () => {
+          "use server";
+          await signIn();
+          redirect(decodeURI(redirectTo));
+        }}
+      >
         <h1>ログイン</h1>
         <button
-          onClick={() =>
-            signIn(
-              "google",
-              {},
-              {
-                prompt: "login",
-                callbackUrl: new URL(
-                  decodeURI(redirectTo),
-                  window.location.origin
-                ).toString(),
-              }
-            )
-          }
-          disabled={status === "loading"}
+        // onClick={() =>
+        //   signIn(
+        //     "google",
+        //     {},
+        //     {
+        //       prompt: "login",
+        //       callbackUrl: new URL(
+        //         decodeURI(redirectTo),
+        //         window.location.origin
+        //       ).toString(),
+        //     }
+        //   )
+        // }
+        // disabled={status === "loading"}
         >
           ログイン
         </button>
-      </div>
+      </form>
     </>
   );
 }
