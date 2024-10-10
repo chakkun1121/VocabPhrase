8041c53feccfae6ad1dbc3459a980028a55b4428
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-10-10 03:47:19 +0000

    change

--- src/app/[lang]/(app)/[fileId]/speaking/page.tsx
+++ src/app/[lang]/(app)/[fileId]/speaking/page.tsx
@@ -1,15 +1,29 @@
-import { redirect } from "next/navigation";
+import { notFound } from "next/navigation";
 import { speakingMode } from "./menu";
 import Link from "next/link";
 import { getTranslation } from "@/app/i18n/server";
+import Speaking from "@/components/functional/speaking";
 
-export default async function Speaking({
+export default async function Page({
   params: { lang, fileId },
+  searchParams: { mode },
 }: {
   params: { lang: "ja" | "en"; fileId: string };
+  searchParams: { mode?: string };
 }) {
-  if (!fileId) redirect("/app");
+  if (!fileId) notFound();
   const { t } = await getTranslation(lang);
+  if (mode) {
+    return (
+      <>
+        {/* 録音を強制的に止めるためにaタグを使用 */}
+        <a href="./speaking" className="underline text-blue-800">
+          {t("speaking:backToSelect")}
+        </a>
+        <Speaking fileId={fileId} mode={mode} lang={lang} />
+      </>
+    );
+  }
   return (
     <>
       <h2 className="text-2xl">{t("speaking:title")}</h2>
@@ -20,7 +34,7 @@ export default async function Speaking({
             <li key={mode.id}>
               <Link
                 className="text-xl underline text-blue-800"
-                href={`./speaking/${mode.id}`}
+                href={`./speaking?mode=${mode.id}`}
                 prefetch={false}>
                 {mode.name[lang]}
               </Link>
