668f0246b8702f621f01135e5f1a40959ba5a5c9
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-10-11 03:53:45 +0000

    fix 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,20 @@
 import Header from "@/components/layouts/header";
+import "./globals.css";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <>
-      <Header />
-      <div className="text-center">Not Found</div>
-    </>
+    <html>
+      <body>
+        <Header />
+        <main className="flex flex-col justify-center items-center gap-4 p-4">
+          <h1 className="text-center text-4xl">404 Not Found</h1>
+          <Button asChild variant="outline">
+            <Link href="/">Home</Link>
+          </Button>
+        </main>
+      </body>
+    </html>
   );
 }
